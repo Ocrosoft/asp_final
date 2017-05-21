85d29e94edf279acff2bd8e3af1ff95226a0e3ed
Ocrosoft <ocrosoft@ocrosoft.com>
2017-05-21 16:15:34 +0800

    设置更改。

--- Data_Access _Layer/Properties/AssemblyInfo.cs
+++ Data_Access _Layer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过下列特性集
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Data_Access__Layer")]
+[assembly: AssemblyTitle("Data_Access_Layer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Data_Access__Layer")]
+[assembly: AssemblyProduct("Data_Access_Layer")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
